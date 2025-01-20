9c5f09c1ffb4ee5f18ceefe3330e60e64521899f
Rybis02 <rabischawarri@gmail.com>
2025-01-20 13:58:14 +0100

    completed 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let date_time = new Date()
 
 let persons = [
@@ -25,6 +27,11 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+const generateId = () => {
+    const id = Math.random(5, 1000000)
+    return id
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Welcome to Phonebook</h1>')
@@ -53,6 +60,29 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
