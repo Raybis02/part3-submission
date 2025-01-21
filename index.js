b1c605220769599fad216f3dd1f842b3ca4935ab
Rybis02 <rabischawarri@gmail.com>
2025-01-21 19:39:37 +0100

    completed 3.8 and fixed issues

--- index.js
+++ index.js
@@ -2,38 +2,49 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+morgan.token('json', (request) => {
+    if (request.method === 'POST') {
+        const json = JSON.stringify(request.body)
+        return json
+    }
+    return ''
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+
 
 let date_time = new Date()
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 const generateId = () => {
-    const id = Math.random(5, 1000000)
+    const id = Math.floor(Math.random() * 10000)
     return id
 }
+
+app.get('/favicon.ico', (req, res) => res.status(204).end())
 
 app.get('/', (request, response) => {
     response.send('<h1>Welcome to Phonebook</h1>')
@@ -52,9 +63,9 @@ app.get(`/api/persons/:id`, (request, response) => {
     const person = persons.find(person => person.id === id)
     if (person) {
         response.json(person)
-    } else 
-        response.send(`<h2>404 Not Found</h2> <p>there is no entry for a person with id ${id}</p>`)
-        response.status(404).end()
+    } else{
+        response.status(404).send(`<h2>404 Page Not Found</h2> <p>there is no entry for a person with id ${id}</p>`).end()
+    }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -67,7 +78,7 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if(!body.name) {
+    if (!body.name) {
         return response.status(400).json({
             error: 'name missing'
         })
